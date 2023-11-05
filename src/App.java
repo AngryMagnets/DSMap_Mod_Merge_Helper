d1f05df5fadf85178c22cc5f933f56f35c06eefc
Liam Goel <liam.goel0678@gmail.com>
2023-11-04 23:33:39 -0700

    Realized I was being dumb about trying to do all the organization in one function
    Anyways it should work now

--- src/App.java
+++ src/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main(String[] args) throws Exception 
     {
         testInit();
-        // fo.organizeModFiles();
+        fo.organizeModFiles();
 
         // Epic
     }
--- src/File_Organizer.java
+++ src/File_Organizer.java
@@ -17,14 +17,14 @@ public class File_Organizer
 	}
 	public void organizeModFiles ()
 	{
-		organizeModFiles(BaseModPath, MergeModPath, "");
+		organizeSharedModFiles(BaseModPath, MergeModPath, "");
 	}
-	public boolean organizeModFiles (File baseDir, File mergeDir, String trackedPath)
+
+	public void organizeSharedModFiles (File baseDir, File mergeDir, String trackedPath)
 	{
 		// Get all sub files and dirs for base and merge mod in the target directory
 		File[] baseSubFiles = baseDir.listFiles()
 			 , mergeSubFiles = mergeDir.listFiles();
-		File targetDir = null;
 
 		for (int bIdx = 0; bIdx < baseSubFiles.length; bIdx++) // Get the file at the nth idx of the base files
 		{
@@ -37,19 +37,19 @@ public class File_Organizer
 					{
 						// System.out.println("directory");
 						trackedPath += "\\" + baseSubFiles[bIdx].getName(); // Add information to currentFilePath
-						organizeModFiles(baseSubFiles[bIdx], mergeSubFiles[mIdx], trackedPath); // Recurse function on appropriate sub directories
+						organizeSharedModFiles(baseSubFiles[bIdx], mergeSubFiles[mIdx], trackedPath); // Recurse function on appropriate sub directories
 					}
 					else // If it is a matching file between base and merge mods
 					{
 						// System.out.println("file");
 						try
 						{
-							FileUtils.moveToDirectory( baseSubFiles[bIdx] // Move matching base file to proper output directory
-													 , new File(BaseSharedOutPath.getAbsolutePath() + trackedPath)
+							FileUtils.moveToDirectory(baseSubFiles[bIdx] // Move matching base file to proper output directory
+													, new File(BaseSharedOutPath.getAbsolutePath() + trackedPath)
+													, true);
+							FileUtils.moveToDirectory(mergeSubFiles[mIdx] // Move matching merge file to proper output directory
+													, new File(MergeSharedOutPath.getAbsolutePath() + trackedPath)
 													 , true);
-							FileUtils.moveToDirectory( mergeSubFiles[mIdx] // Move matching merge file to proper output directory
-													 , new File(MergeSharedOutPath.getAbsolutePath() + trackedPath)
-													 , true);	
 						}
 						catch (IOException ioe)
 						{
@@ -59,52 +59,81 @@ public class File_Organizer
 					}
 				}
 			}
+		}
+		/*\ 	// If no merge file/directories match the checked base file
+		// 	targetDir = new File(UniqueOutPath.getName() + "\\" + trackedPath); // Debug
+		// 	System.out.println(targetDir.getAbsolutePath());
+		// 	try
+		// 	{
+		// 		System.out.println(baseSubFiles[bIdx].getName()); // Debug
+		// 		// Move to UniqueOutPath
+		// 		FileUtils.moveToDirectory(baseSubFiles[bIdx], targetDir, true); 
+		// 	}
+		// 	catch (IOException ioe)
+		// 	{
+		// 		System.err.println("Error moving " + baseSubFiles[bIdx].getName() + " to proper directory");
+		// 		ioe.printStackTrace();
+		// 	}
+		} 
+		// // At this point there should be no more base files in the current directory
+		// mergeSubFiles = mergeDir.listFiles(); // Update to remaining merge files 
+		// for (File f : mergeSubFiles) // Move remaining files to UniqueOutPath in appropriate sub directory
+		// {
+		// 	try
+		// 	{
+		// 		FileUtils.moveToDirectory(f, targetDir, true); 
+		// 	}
+		// 	catch (IOException ioe)
+		// 	{
+		// 		System.err.println("Error moving " + f.getName() + " to proper directory");
+		// 		ioe.printStackTrace();
+		// 	}	
+		\*/
+		moveRemainingFiles(); 
+	}
 
-			// If no merge file/directories match the checked base file
-			targetDir = new File(UniqueOutPath.getName() + "\\" + trackedPath); // Debug
-			System.out.println(targetDir.getAbsolutePath());
+	private void moveRemainingFiles ()
+ 	{
+		for (File bf : BaseModPath.listFiles()) 
+		{
+			
 			try
 			{
-				System.out.println(baseSubFiles[bIdx].getName());
-				// Move to UniqueOutPath
-				FileUtils.moveToDirectory(baseSubFiles[bIdx], targetDir, true); 
+				FileUtils.copyToDirectory(bf, UniqueOutPath);
+				bf.delete();
 			}
 			catch (IOException ioe)
 			{
-				System.err.println("Error moving " + baseSubFiles[bIdx].getName() + " to proper directory");
 				ioe.printStackTrace();
 			}
-		} 
-		// At this point there should be no more base files in the current directory
-		mergeSubFiles = mergeDir.listFiles(); // Update to remaining merge files 
-		for (File f : mergeSubFiles) // Move remaining files to UniqueOutPath in appropriate sub directory
+		}
+		for (File mf : MergeModPath.listFiles()) 
 		{
 			try
 			{
-				FileUtils.moveToDirectory(f, targetDir, true); 
+				FileUtils.copyToDirectory(mf, UniqueOutPath);
+				mf.delete();
 			}
 			catch (IOException ioe)
 			{
-				System.err.println("Error moving " + f.getName() + " to proper directory");
 				ioe.printStackTrace();
-			}	
+			}
 		}
-		return true; // Return 
 	}
 
 	public void clearDirectories () throws IOException
 	{
 		for (File f : BaseSharedOutPath.listFiles()) 
 		{
-			FileUtils.delete(f);
+			FileUtils.forceDelete(f);
 		}
 		for (File f : MergeSharedOutPath.listFiles()) 
 		{
-			FileUtils.delete(f);
+			FileUtils.forceDelete(f);
 		}
 		for (File f : UniqueOutPath.listFiles()) 
 		{
-			FileUtils.delete(f);
+			FileUtils.forceDelete(f);
 		}
 	}
 }
