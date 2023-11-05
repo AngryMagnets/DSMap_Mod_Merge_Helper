da62cdaea1ad16983db5be9cf244d70c37f9ff55
Liam Goel <liam.goel0678@gmail.com>
2023-11-04 21:44:39 -0700

    Recursive Bug + Test Functions
    Only puts unique files in the very last directory in the unique output folder

--- src/App.java
+++ src/App.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.*;
+
 public class App 
 {
     public static void main(String[] args) throws Exception 
@@ -7,11 +13,67 @@ public class App
         , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModPath"
         , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\BaseModSharedOutput"
         , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModSharedOutput"
-        , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModUniqueOutput");
+        , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\UniqueOutput");
 
-        // fo.clearDirectories();
-        fo.saveSameFiles();
+        fo.organizeModFiles();
 
         // Epic
+    }
+
+    File_Organizer fo;
+    @Before
+    public void testInit () throws IOException
+    {
+        fo = new File_Organizer
+        ( "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\BaseModPath"
+        , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModPath"
+        , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\BaseModSharedOutput"
+        , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModSharedOutput"
+        , "D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\UniqueOutput");
+
+        File bm = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\BaseModPath")
+           , mm = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModPath")
+           , bo = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\BaseModSharedOutput")
+           , mo = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModSharedOutput")
+           , uo = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\UniqueOutput")
+           , bt = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\BaseModTestFiles")
+           , mt = new File("D:\\Documents\\Scripts\\Java Projects\\DSMap_Mod_Merge_Helper\\MergeModTestFiles");
+        
+           for (File bf : bm.listFiles()) 
+           {
+                FileUtils.forceDelete(bf);
+           }
+           for (File mf : mm.listFiles()) 
+           {
+                FileUtils.forceDelete(mf);
+           }
+
+           for (File of : bo.listFiles()) 
+           {
+                FileUtils.forceDelete(of);
+           }
+           for (File of : mo.listFiles()) 
+           {
+                FileUtils.forceDelete(of);
+           }
+           for (File of : uo.listFiles()) 
+           {
+                FileUtils.forceDelete(of);
+           }
+
+           for (File bf : bt.listFiles()) 
+           {
+                FileUtils.copyDirectoryToDirectory(bf, bm);
+           }
+           for (File mf : mt.listFiles()) 
+           {
+                FileUtils.copyDirectoryToDirectory(mf, mm);
+           }
+    }
+
+    @Test 
+    public void testFileOrganization () throws Exception
+    {
+        fo.organizeModFiles();
     }
 }
--- src/File_Organizer.java
+++ src/File_Organizer.java
@@ -15,59 +15,10 @@ public class File_Organizer
 		MergeSharedOutPath = new File(mop);
 		UniqueOutPath = new File(nop);
 	}
-	
-	public boolean saveSameFiles ()
-	{	
-		File[] pbsub, pmsub, pbsubfiles, pmsubfiles;
-		try 
-		{
-			for (File f : MergeModPath.listFiles())
-			{
-				FileUtils.copyToDirectory(f, UniqueOutPath);
-			}
-			pbsub = BaseModPath.listFiles(); pmsub = UniqueOutPath.listFiles();
-			for (int i = 0; i < pbsub.length; i++)
-			{
-				pbsubfiles = pbsub[i].listFiles();
-				
-				for (int j = 0; j < pmsub.length; j++)
-				{
-					pmsubfiles = pmsub[j].listFiles();
-					if (!pbsub[i].getName().equals(pmsub[j].getName()))
-					{
-						continue;
-					}
-					for (int k = 0; k < pbsubfiles.length; k++)
-					{
-						for (int l = 0; l < pmsubfiles.length; l++)
-						{
-							if (pbsubfiles[k].getName().equals(pmsubfiles[l].getName()))
-							{
-								FileUtils.copyToDirectory(pbsubfiles[k], new File(BaseSharedOutPath.getName() 
-								+ "\\" + pbsub[i].getName()));
-								FileUtils.moveFileToDirectory(pmsubfiles[l], new File(MergeSharedOutPath.getName() 
-								+ "\\" + pmsub[j].getName()), true);
-							}
-							if (l == pmsubfiles.length - 1 && pmsub[j].list() == null)
-							{
-								FileUtils.deleteDirectory(pmsub[j]);
-							}
-						}
-					}
-				}
-			}
-		}
-		catch (IndexOutOfBoundsException ioobe)
-		{
-			System.out.println("Stoopid idjhut cam't pwogwam");
-		}
-		catch (Exception e)
-		{
-			System.err.println("Check input and output file paths");
-		}
-		return false;
+	public void organizeModFiles ()
+	{
+		organizeModFiles(BaseModPath, MergeModPath, "");
 	}
-
 	public boolean organizeModFiles (File baseDir, File mergeDir, String trackedPath)
 	{
 		// Get all sub files and dirs for base and merge mod in the target directory
@@ -75,18 +26,22 @@ public class File_Organizer
 			 , mergeSubFiles = mergeDir.listFiles();
 
 		for (int bIdx = 0; bIdx < baseSubFiles.length; bIdx++) // Get the file at the nth idx of the base files
-		{	
+		{
 			for (int mIdx = 0; mIdx < mergeSubFiles.length; mIdx++) // Get the file at the mth idx of the merge files
 			{
+				// System.out.print(baseSubFiles[bIdx].getName() + " == " + mergeSubFiles[mIdx].getName() + " ");
 				if (baseSubFiles[bIdx].getName().equals(mergeSubFiles[mIdx].getName())) // if file names match
 				{
+					// System.out.print("true ");
 					if (baseSubFiles[bIdx].isDirectory()) // If it is a directory
 					{
+						System.out.println("directory");
 						trackedPath += "\\" + baseSubFiles[bIdx].getName(); // Add information to currentFilePath
 						return organizeModFiles(baseSubFiles[bIdx], mergeSubFiles[mIdx], trackedPath); // Recurse function on appropriate sub directories
 					}
 					else // If it is a matching file between base and merge mods
 					{
+						// System.out.println("file");
 						try
 						{
 							FileUtils.moveToDirectory( baseSubFiles[bIdx] // Move matching base file to proper output directory
@@ -103,7 +58,8 @@ public class File_Organizer
 						}	
 					}
 				}
-			}	
+			}
+			// System.out.print("false ");	
 			// If no merge file/directories match the checked base file
 			try
 			{
@@ -112,13 +68,13 @@ public class File_Organizer
 			}
 			catch (IOException ioe)
 			{
-				System.err.println("Error moving " + baseSubFiles[bIdx].getName() + " to proper directory");
+				// System.err.println("Error moving " + baseSubFiles[bIdx].getName() + " to proper directory");
 				ioe.printStackTrace();
 			}
 		} // At this point there should be no more base files in the current directory
 		
-		mergeSubFiles = mergeDir.listFiles(); // update to remaining merge files 
-		for (File f : mergeSubFiles) 
+		mergeSubFiles = mergeDir.listFiles(); // Update to remaining merge files 
+		for (File f : mergeSubFiles) // Move remaining files to UniqueOutPath in appropriate sub directory
 		{
 			try
 			{
@@ -126,30 +82,13 @@ public class File_Organizer
 			}
 			catch (IOException ioe)
 			{
-				System.err.println("Error moving " + baseSubFiles[bIdx].getName() + " to proper directory");
+				System.err.println("Error moving " + f.getName() + " to proper directory");
 				ioe.printStackTrace();
 			}	
 		}
-		return true;
+		return true; // Return 
 	}
-		/*\
-		 * if (match & file)
-		 * {
-		 * 	baseFile.sendToAppropriateSubDirInBaseSharedOutPath DO NOT COPY, only move
-		 * 	mergeFile.sendToAppropriateSubDirInMergeSharedOutPath DO NOT COPY, only move
-		 * }
-		 * if (noMatchesForNthBaseFile/Dir)
-		 * {
-		 * 	baseFile.sendToAppropriateSubDirInUniqueOutPath DO NOT COPY, only move
-		 * }
-		 * 
-		 * 
-		 * Once (baseSubFiles is empty AND mergeFiles remain)
-		 * {
-		 * 	mergeFiles.sendToAppropriateSubDirInUniqueOutPath DO NOT COPY, only move
-		 * 	return true;
-		 * }
-		\*/	
+
 	public void clearDirectories () throws IOException
 	{
 		for (File f : BaseSharedOutPath.listFiles()) 
